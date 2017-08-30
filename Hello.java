4ea018a6495ab6bae4774ec7705dc9a4c057855a
Adichart.C <adichart.c@motiftech.com>
2017-08-30 19:04:48 +0700

    Hello World!

--- Hello.java
+++ Hello.java
@@ -5,11 +5,16 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Adichart();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
     }
+	
+	private void Adichart() {
+		System.out.println("Adichart Chanhomhaun, SE");
+	}
     
     public static void main(String [] argv) {
         new Hello();
