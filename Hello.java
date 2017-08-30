852141767273a9c65ae6d4ec61bc1226ff110511
Sujinno Khongmaung <sujinno.k@motiftech.com>
2017-08-30 19:33:38 +0700

    modify test12

--- Hello.java
+++ Hello.java
@@ -5,11 +5,16 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Sujinno();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("  Demo  - Krerk Piromsopa, Ph.D.");
+    }
+	
+	private void Sujinno() {
+        System.out.println("My name is Sujinno.K");
     }
     
     public static void main(String [] argv) {
