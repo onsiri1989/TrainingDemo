7dac198dfdf8be0c92c30acf439eb6cba932d1e7
Panjapol Lomarat <panjapol.l@motiftech.com>
2017-08-30 18:47:20 +0700

    Add .gitignore for java project and add function to print Panjapoll in Hello

--- Hello.java
+++ Hello.java
@@ -5,10 +5,15 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+        this.Panjapoll();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+    }
+
+    private void Panjapoll() {
+        System.out.println("Trainee - Panjapol Lomarat, SSE");
     }
     
     public static void main(String [] argv) {
