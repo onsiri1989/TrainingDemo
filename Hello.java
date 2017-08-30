bf53ccd01f13ed28ffbdf35b51b6dd904a5e812b
Sittikorn.C <sittikorn.c@motiftech.com>
2017-08-30 19:32:37 +0700

    Sittikorn.C Commit Hello.class

--- Hello.java
+++ Hello.java
@@ -5,11 +5,17 @@ class Hello {
     public Hello() {
         System.out.println("Hello! We are:");
         this.Krerk();
+		this.Sittikorn();
     }
     
     private void Krerk() {
         System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
         System.out.println("  Demo  - Krerk Piromsopa, Ph.D.");
+    }
+	
+	private void Sittikorn() {
+        System.out.println("Trainer - Sittikorn Chuensukprasong, Software Engineer.");
+        System.out.println("  Demo  - Sittikorn Chuensukprasong, Software Engineer.");
     }
     
     public static void main(String [] argv) {
